efb97649df656fea0dbe918015700e7cbb62e71f
Zak Kraimeche <zkraime@gmail.com>
2015-08-21 06:24:28 -0400

    functionality done TODO: -UI styling -Custom email templates

--- cosAdminInterface/cosAdminInterface/settings.py
+++ cosAdminInterface/cosAdminInterface/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'openscienceframeworktest@gmail.com'
+EMAIL_HOST_PASSWORD = 'cospassword'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
